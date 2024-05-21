641deb2226d5ba301037e4ad87731dbf6fb33f83
Tharushi  De Silva <73363789+TharushiDSilva@users.noreply.github.com>
2024-05-21 15:07:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,23 @@ app.get('/projects', async (req, res) => {
         res.status(500).json({ message: err.message });
     }
 });
+app.post('/projects', async (req, res) => {
+    try {
+        const newprojects = await Project.save();
+        res.json(newprojects);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+app.get('/blogs', async (req, res) => {
+    try {
+        const blogs = await blog.find();
+        res.json(projects);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
